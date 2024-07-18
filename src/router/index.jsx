7ffb5df91cc5c10c908ff65236cdc346e7445975
desarrollo4@historyclinical.com <desarrollo4@historyclinical.com>
2024-07-18 16:44:15 -0500

    added main component to root path

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { MainComponent } from "../components/MainComponent";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainComponent />,
+    errorElement: <div> Hubo un error!!</div>,
+  },
+]);
 
 export const CustomRouterProvider = () => (
   <RouterProvider router={router}></RouterProvider>
